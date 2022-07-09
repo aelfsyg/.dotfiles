e72893b49b7b28620245f0d9e8c55239d7ca4129
Aelfsyg <aelfsyg@gmail.com>
2022-07-09 10:37:26 +0100

    Use custom doom-nord theme.

--- doom/.config/doom/themes/doom-one-theme.el
+++ doom/.config/doom/themes/doom-one-theme.el
@@ -56,7 +56,7 @@ Can be an integer to determine the exact padding."
    ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
    ;; or region), especially when paired with the `doom-darken', `doom-lighten',
    ;; and `doom-blend' helper functions.
-   (bg-alt     (doom-darken '("#21242b" "black"       "black") 0.5))
+   (bg-alt     (doom-darken '("#21242b" "black"       "black") 0.4))
    (fg-alt     '("#5B6268" "#2d2d2d"     "white"        ))
 
    ;; These should represent a spectrum from bg to fg, where base0 is a starker
