f3da3f1187236cd2d47e0d70a71e34b3d10862cb
Aelfsyg <aelfsyg@gmail.com>
2022-10-19 21:06:08 +0100

    Enable csharp and fsharp Emacs modules.

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -122,7 +122,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;; csharp              ; unity, .NET, and mono shenanigans
+       csharp              ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
@@ -133,7 +133,7 @@
        ;;ess               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
-       ;;fsharp            ; ML stands for Microsoft's Language
+       fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        (go +lsp)           ; the hipster dialect
